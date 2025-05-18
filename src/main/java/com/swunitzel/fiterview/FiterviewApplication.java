38d08c15b7109af3e28945bc24663e6d11dc85f8
westzerosilver <oooiooop9988@gmail.com>
2025-05-18 15:24:25 +0900

    Chore: init

---
+++ src/main/java/com/swunitzel/fiterview/FiterviewApplication.java
@@ -0,0 +1,13 @@
+package com.swunitzel.fiterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swunitzel/fiterview/FiterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swunitzel.fiterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
